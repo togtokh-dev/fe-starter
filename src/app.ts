a5b004773b43275687a18beb23340bce0c8615ae
buyantogtokh <buyantogtokh.alt@unitel.mn>
2023-05-29 13:49:40 +0800

    main

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,10 @@ app.use(
     express.json();
   }
 );
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(function (req, res) {
+  console.log(req.host);
+  express.static(path.join(__dirname, "../public"));
+});
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public"));
